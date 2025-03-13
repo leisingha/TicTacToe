d522099ad24241ad86fcb5ae213dfe46db865733
leisingha <120764124+leisingha@users.noreply.github.com>
2025-03-13 19:25:09 -0400

    gameController update

--- script.js
+++ script.js
@@ -7,14 +7,28 @@ function createPlayer (name) {
 
 function createHumanPlayer (name){
     const user = createPlayer(name);
-    const getAction = () => prompt('Enter X or O');
-    return Object.assign({}, user, {getAction});
+    const getPosition = () => {
+        const input = prompt('Pick Cell (format: [row][col])');
+        const match = input.match(/\[(\d+)\]\[(\d+)\]/);
+        if (match) {
+            const row = parseInt(match[1], 10);
+            const col = parseInt(match[2], 10);
+            return { row, col };
+        } else {
+            alert('Invalid format. Please enter in the format [row][col]');
+            return getPosition();
+        }
+    };
+    return Object.assign({}, user, {getPosition});
 }
 
 function createComputerPlayer (name){
     const {name, changeTurn, isTurn} = createPlayer(name);
-    const getAction = () => prompt('Enter X or O');
-    return {name, changeTurn, isTurn, getAction};
+    const getPosition = () => {
+        const input = prompt('Pick Cell');
+        return input;
+    };
+    return {name, changeTurn, isTurn, getPosition};
 }
 
 function createCell () {
@@ -38,9 +52,15 @@ function GameBoard ()  {
         boardArray.push(row);
     }
 
-    const getBoard = () => boardArray;
+    const getBoard = () => console.log(boardArray);
 
-    const modifyCell = (pos, val) => boardArray[pos].addVal(val);
+    const modifyCellX = (pos) => {
+        boardArray[pos].addVal('X');
+    }
+
+    const modifyCellO = (val) => {
+        boardArray[pos].addVal('O');
+    }
 
     function allCellEqual(outerArray) {
         let streak = false;
@@ -58,7 +78,7 @@ function GameBoard ()  {
         return streak;
     }
 
-    const isStreak = () => {
+    const isStreak =() => {
         let streak = false;
         //check horizontal streak
         streak = allCellEqual(boardArray);
@@ -77,14 +97,45 @@ function GameBoard ()  {
         return streak;
     }
 
-    return{getBoard, modifyCell, isStreak}
+    return{getBoard, modifyCellX, modifyCellO, isStreak}
 }
 
 function gameController () {
     const player1 = createHumanPlayer('Lei');
     const player2 = createComputerPlayer('AI');
 
-    const roundTurn = () => 
+    const board = GameBoard();
+
+    const playGame = () => {
+        while (!board.isStreak){
+            playRound();
+        }
+    }
+
+   
+
+    const playRound = () => {
+        //p1 turn
+        player1.changeTurn();
+        board.modifyCellX(player1.getPosition());
+        player1.changeTurn();
+        board.getBoard();
+        if (board.isStreak){
+            console.log('GAME OVER!!!');
+        }
+
+        //p2 turn
+        player2.changeTurn();
+        board.modifyCellO(player2.getPosition());
+        player2.changeTurn();
+        board.getBoard();
+        if (board.isStreak){
+            console.log('GAME OVER!!!');
+        }
+
+    }
+
+    return {playGame}
 }
 
 
